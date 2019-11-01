b535926053a5b3e36236927b0f957a326838fee8
itma96 <andrei.ionescu.tautu@gmail.com>
2019-11-01 17:30:27 +0200

    [add] added endpoint method for downloading blob objects

--- src/main/java/posd/proiect/GoogleStorageService.java
+++ src/main/java/posd/proiect/GoogleStorageService.java
@@ -1,5 +1,6 @@
 package posd.proiect;
 
+import com.google.cloud.storage.BlobId;
 import org.springframework.stereotype.Service;
 
 import com.google.auth.oauth2.ServiceAccountCredentials;
@@ -51,5 +52,35 @@ public class GoogleStorageService {
             bucketList.add(bucket);
         }
         return bucketList;
+    }
+
+    public Bucket getBucket(String bucketName) {
+        com.google.cloud.storage.Bucket currentBucket = gcs.get(bucketName);
+        if (currentBucket != null) {
+            Bucket bucket = new Bucket();
+            bucket.setName(currentBucket.getName());
+            List<Blob> blobList = new ArrayList<>();
+            for (com.google.cloud.storage.Blob currentBlob : currentBucket.list().iterateAll()) {
+                Blob blob = new Blob();
+                blob.setId(currentBlob.getGeneratedId());
+                blob.setName(currentBlob.getName());
+                blob.setContentType(currentBlob.getContentType());
+                blob.setHash(currentBlob.getMd5ToHexString());
+                blob.setSize(currentBlob.getSize());
+                blob.setCreationTime(currentBlob.getCreateTime());
+                blobList.add(blob);
+            }
+            bucket.setBlobs(blobList);
+            return bucket;
+        }
+        return null;
+    }
+
+    public byte[] getFile(String bucketName, String fileName) {
+        com.google.cloud.storage.Blob currentBlob = gcs.get(BlobId.of(bucketName, fileName));
+        if (currentBlob != null) {
+            return currentBlob.getContent();
+        }
+        return null;
     }
 }
--- src/main/java/posd/proiect/ServiceEndpoint.java
+++ src/main/java/posd/proiect/ServiceEndpoint.java
@@ -1,12 +1,13 @@
 package posd.proiect;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.net.URISyntaxException;
 import java.util.List;
 
 @Component
@@ -25,7 +26,41 @@ public class ServiceEndpoint {
     @GET
     @Path("/buckets")
     @Produces("application/json")
-    public List<Bucket> listBuckets() {
-        return storageService.getAllBuckets();
+    public Response listBuckets() {
+        List<Bucket> bucketList = storageService.getAllBuckets();
+        if (bucketList == null) {
+            return Response.status(404).build();
+        }
+        return Response
+                .status(200)
+                .entity(bucketList)
+                .build();
+    }
+
+    @GET
+    @Path("/bucket")
+    @Produces("application/json")
+    public Response getBucket(@QueryParam("name") String name) {
+        Bucket bucket = storageService.getBucket(name);
+        if(bucket == null) {
+            return Response.status(404). build();
+        }
+        return Response
+                .status(200)
+                .entity(bucket)
+                .build();
+    }
+
+    @GET
+    @Path("/download")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response download(@QueryParam("bucket") String bucketName, @QueryParam("file") String fileName) {
+        byte[] data = storageService.getFile(bucketName, fileName);
+        if (data == null) {
+            return Response.status(404).build();
+        }
+        return Response.ok(data, MediaType.APPLICATION_OCTET_STREAM_TYPE)
+                .header("Content-Disposition", "attachment; filename=\"" + fileName + "\"")
+                .build();
     }
 }
